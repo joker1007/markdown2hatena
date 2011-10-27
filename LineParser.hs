1f9f2d8b4c475ed5668cabea4cc6a0510f54c8e8
joker1007 <kakyoin.hierophant@gmail.com>
2011-10-27 22:24:58 +0900

    ライセンスについて記述

--- LineParser.hs
+++ LineParser.hs
@@ -1,3 +1,14 @@
+--
+-- $Id: LineParser.hs,v 1.4 2006/04/09 20:40:24 aamine Exp $
+--
+-- Copyright (c) 2005,2006 Minero Aoki
+--
+-- This program is free software.
+-- You can distribute/modify this program under the terms of
+-- the GNU LGPL, Lesser General Public License version 2.1.
+-- For details of the GNU LGPL, see the file "COPYING".
+--
+
 module LineParser
   (LineParser, indented, blank, notBlank, lineSatisfy, firstChar) where
 
--- Node.hs
+++ Node.hs
@@ -1,3 +1,14 @@
+--
+-- markdown2hatena
+--
+-- Copyright (c) 2011 Tomohiro Hashidate
+--
+-- This program is free software.
+-- You can distribute/modify this program under the terms of
+-- the GNU LGPL, Lesser General Public License version 2.1.
+-- For details of the GNU LGPL, see the file "COPYING".
+--
+
 module Node
   (Node(..), compile) where
 
